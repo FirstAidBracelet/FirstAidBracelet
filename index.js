3549ce0d503a03a035e1ff06b1478c52b8ab8fc5
FirstAidBracelet <firstaidbracelet@gmail.com>
2017-04-03 20:09:50 +0300

    added mongo.ejs

--- index.js
+++ index.js
@@ -19,10 +19,11 @@ app.get('/db', function (request, response) {
   });
 });
 
-/*
-mongodb stuff
+
+//mongodb stuff
 app.get('/mongo', function (request, response) {
-    var MongoClient = require('mongodb').MongoClient
+  response.render('pages/mongo');
+    /*var MongoClient = require('mongodb').MongoClient
     , assert = require('assert');
 
     // Connection URL
@@ -33,10 +34,10 @@ app.get('/mongo', function (request, response) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
     //
-    //bresponse.render('pages/mongo');
-    db.close();
-    });
-};*/
+    response.render('pages/mongo');
+    //db.close();
+    //});*/
+};
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
