02935ce591b5de5b3291d7bccea25b98abecab47
FirstAidBracelet <firstaidbracelet@gmail.com>
2017-04-03 20:02:00 +0300

    added mongo.ejs

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 
+/*
+postgres stuff
+*/
+
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
@@ -15,6 +19,24 @@ app.get('/db', function (request, response) {
   });
 });
 
+/*
+mongodb stuff*/
+app.get('/mongo', function (request, response) {
+    var MongoClient = require('mongodb').MongoClient
+    , assert = require('assert');
+
+    // Connection URL
+    var url = 'mongodb://heroku_8lwbv1x0:hlus7a54o0lnapqd2nhtlkaet7@dbh73.mlab.com:27737/heroku_8lwbv1x0';
+
+    // Use connect method to connect to the server
+    MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+    //
+    response.render('pages/mongo');
+    db.close();
+    });
+};
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
