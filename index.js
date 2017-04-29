17f30a0bf35a47aab164570053be0748c3e12cc9
alexvitho <alexvitho@gmail.com>
2017-04-29 19:52:26 +0300

    Learning on click events

--- index.js
+++ index.js
@@ -64,9 +64,9 @@ app.get('/doctor', function(request, response) {
 
 
 app.get('/mainPage', function(request, response) {
-    mainPage.filters.push("one");
-    mainPage.filters.push("two");
-    mainPage.filters.push("three");
+  //  mainPage.filters.push("one");
+  //  mainPage.filters.push("two");
+  //  mainPage.filters.push("three");
      response.render('pages/mainPage', { mainPage: mainPage });
 });
 
