d81622a3fac9a7baa8ffdf26b36c210577c739eb
alexvitho <alexvitho@gmail.com>
2017-04-30 21:46:27 +0300

    list contains try- from ejs

--- mainPage.js
+++ mainPage.js
@@ -1,11 +1,6 @@
 ﻿var filters = [];
 
 var addFilter = function (fltr) {
-    for (i = 0; i < filters.length; i++) {
-        if (fltr === filters[i]) {
-            return;
-        }
-    }
     filters.push(fltr);
 };
 
