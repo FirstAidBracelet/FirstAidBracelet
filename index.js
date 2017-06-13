57dab4363a711803f031a2c31d81f93f5a4e2a6c
alexvitho <alexvitho@gmail.com>
2017-06-14 01:01:41 +0300

    Refactoring index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var app = express();
-//var server = require('http').Server(app);
-//var io = require('socket.io')(server);
 var pg = require('pg');
 var mongoUrl = 'mongodb://heroku_8lwbv1x0:hlus7a54o0lnapqd2nhtlkaet7@dbh73.mlab.com:27737/heroku_8lwbv1x0';
 var MongoClient = require('mongodb').MongoClient
@@ -11,60 +9,64 @@ var cookieParser = require('cookie-parser');
 const EventEmitter = require('events');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-/*
-postgres stuff
-*/
+app.set('port', (process.env.PORT || 5000)); // Defining Application port
+var io = require('socket.io').listen(app.listen(app.get('port'))); // Setting App and Socket Listening port (same one)
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function (request, response) {
+    response.render('pages/index');
+});
 
 app.get('/mongo', function (request, response) {
-  
-      // Use connect method to connect to the server
-    MongoClient.connect(mongoUrl, function(err, db) {
-        assert.equal(null, err);
-    var col = db.collection('soldiers');
-    col.find().toArray(function(err, docs) {
-      response.render('pages/mongo', {docs: docs});
-    });
 
-    db.close();
+    MongoClient.connect(mongoUrl, function (err, db) {
+        assert.equal(null, err);
+        var col = db.collection('soldiers');
+        col.find().toArray(function (err, docs) {
+            response.render('pages/mongo', { docs: docs });
+        });
+        db.close();
     });
 });
 
 app.get('/admin', function (request, response) {
-  
-      // Use connect method to connect to the server
-    MongoClient.connect(mongoUrl, function(err, db) {
+
+    MongoClient.connect(mongoUrl, function (err, db) {
         assert.equal(null, err);
-    var equipmentDB = db.collection('equipment');
-    equipmentDB.find().toArray(
-        function(err, docs) {
-            response.render('pages/admin', {docs: docs});
-        }
-    );
-    db.close();
+        var equipmentDB = db.collection('equipment');
+        equipmentDB.find().toArray(
+            function (err, docs) {
+                response.render('pages/admin', { docs: docs });
+            }
+        );
+        db.close();
     });
 });
 
 app.post('/admin', function (request, response) {
-  
-      // Use connect method to connect to the server
-    MongoClient.connect(mongoUrl, function(err, db) {
+
+    MongoClient.connect(mongoUrl, function (err, db) {
         assert.equal(null, err);
+        db.collection('equipment').insertOne({
+            equipment_id: request.body.itemId,
+            name: request.body.name,
+            type: request.body.type
+        })
 
-    db.collection('equipment').insertOne({
-          equipment_id: request.body.itemId,
-          name: request.body.name,
-          type: request.body.type
-    })
+            .then(function (result) {
+                response.redirect('/admin')
+            })
 
-    .then(function(result) {
-        response.redirect('/admin')
-    }) 
-
-    db.close();
+        db.close();
     });
 });
 
@@ -73,31 +75,14 @@ app.post('/admin_delete_item', (req, res) => {
     MongoClient.connect(mongoUrl, function (err, db) {
         assert.equal(null, err);
         db.collection('equipment').findOneAndDelete({ equipment_id: req.body.item_id }, function () {
-            db.collection('equipment').find().toArray(function(err, docs) {
-                res.render('pages/admin', {docs: docs});
+            db.collection('equipment').find().toArray(function (err, docs) {
+                res.render('pages/admin', { docs: docs });
             });
-            db.close(); 
+            db.close();
         });
     });
 });
 
-
-app.set('port', (process.env.PORT || 5000));
-var io = require('socket.io').listen(app.listen(app.get('port')));
-
-
-
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname ));
-
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function (request, response) {
-   response.render('pages/index');
-});
 
 app.get('/login', function (request, response) {
     var type = request.cookies.type;
@@ -108,14 +93,12 @@ app.get('/login', function (request, response) {
         response.redirect('/admin_main');
     }
     else {
-        // Use connect method to connect to the server
         MongoClient.connect(mongoUrl, function (err, db) {
             assert.equal(null, err);
             var col = db.collection('users');
             col.find().toArray(function (err, docs) {
                 response.render('pages/login', { docs: docs });
             });
-
             db.close();
         });
     }
@@ -160,7 +143,7 @@ app.get('/user', function (request, response) {
             var equipmentDB = db.collection('soldiers');
             equipmentDB.find().toArray(
                 function (err, docs) {
-                    response.render('pages/add_user', { docs : docs });
+                    response.render('pages/add_user', { docs: docs });
                 }
             );
             db.close();
@@ -272,7 +255,7 @@ var filtersArray = []; // array that stores the filters for the AND operation
 app.post('/get-soldiers/:filter/:value/:action', function (req, res) {
     var result = [];
     var fltr = { [req.params.filter]: req.params.value }; // Check if there is no duplicated filters
-    if (req.params.action == "add") {     
+    if (req.params.action == "add") {
         for (var i = 0; i < filtersArray.length; i++) {
             if (Object.keys(filtersArray[i])[0] == Object.keys(fltr)[0] && filtersArray[i][Object.keys(filtersArray[i])[0]] == fltr[Object.keys(fltr)[0]]) {
                 return;
@@ -284,7 +267,7 @@ app.post('/get-soldiers/:filter/:value/:action', function (req, res) {
         for (var i = 0; i < filtersArray.length; i++) {
             if (Object.keys(filtersArray[i])[0] == Object.keys(fltr)[0] && filtersArray[i][Object.keys(filtersArray[i])[0]] == fltr[Object.keys(fltr)[0]]) {
                 filtersArray.splice(i, 1);
-                   break;
+                break;
             }
         }
         if (filtersArray.length == 0) {
@@ -309,18 +292,21 @@ app.post('/get-soldiers/:filter/:value/:action', function (req, res) {
         }, function () {
             db.close();
             res.json(result);
-            
+
         });
     });
 });
 
-class MyEmitter extends EventEmitter {  }
+class MyEmitter extends EventEmitter { } // event hendler for post request from android
 const myEmitter = new MyEmitter();
 var client;
-io.sockets.on('connection', function (socket) { client = socket; });
+io.sockets.on('connection', function (socket) { client = socket; }); // the actual socket opening and definition
 
 
-
+/*
+This is the Event function that handles the Android request for
+updating the soldiers table
+*/
 myEmitter.on('event', () => {
     console.log('an event occurred');
     var result = [];
@@ -349,17 +335,20 @@ myEmitter.on('event', () => {
     });
 
 });
-
+/*
+This is the Post request for the Android application,
+It trigers event that update the soldiers table trought the socket
+*/
 app.post('/soldiersChange', function (request, response) {
     myEmitter.emit('event');
-    console.log('an event occurred!');
-    response.send("Got your request")
+    console.log('Got request from android!');
+    response.send("Android i got your request!")
 });
 
 
 app.post('/get-soldier/:braceletId', function (req, res) {
-      var result = [];
-          MongoClient.connect(mongoUrl, function (err, db) {
+    var result = [];
+    MongoClient.connect(mongoUrl, function (err, db) {
         assert.equal(null, err);
         db.collection('soldiers').find({ bracelet_id: req.params.braceletId }).forEach(function (sld, err) {
             assert.equal(null, err);
@@ -370,9 +359,3 @@ app.post('/get-soldier/:braceletId', function (req, res) {
         });
     });
 });
-
-
-//server.listen(3000);
-//app.listen(app.get('port'), function () {
-//    console.log('Node app is running on port', app.get('port'));
-//});
