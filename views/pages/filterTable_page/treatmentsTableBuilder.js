96c26da35be13b84d7699c871fb2b3a5ec2c6c73
alexvitho <alexvitho@gmail.com>
2017-07-18 21:15:46 +0300

    treatments table documentation

--- views/pages/filterTable_page/treatmentsTableBuilder.js
+++ views/pages/filterTable_page/treatmentsTableBuilder.js
@@ -1,12 +1,20 @@
+/*
+Builds the treatments table for given soldier.
+Attention - <% %> iterators are "EJS" syntax. (Read about ejs module)
 
+ @param givenSoldier - array of size 1 that contains exactly one soldier as a legal JSON object.
+ @pageParam treatments_table - array that we pass to soldiersFiltersTable.ejs page when we render it.
+            Each element in it represents header for specific column of the treatments table.
+(This function will be used by  getTreatments() function to build table by given bracelet_ID)
+*/
 function buildTreatmentsTable(givenSoldier) {
 
-    givenSoldier.forEach(function (soldier) {  // this is the found soldier
-        // Here we create Dynamicly linear list
+    givenSoldier.forEach(function (soldier) {  // The found soldier
+        // Here we create Dynamicly linear list element - because treatment tables are arranged in list container.
         var node = document.createElement("LI");
         node.setAttribute('style', 'overflow:scroll; height:225px; width:350px');
         node.setAttribute("id", "eqipId" + soldier.Bracelet_ID);
-        // Here we will insert our table
+        // Here we will create the table for treatment.
         var table = document.createElement('table');
         table.setAttribute("class", "table table-striped");
         var tHeaderr = table.insertRow(0);
@@ -80,12 +88,21 @@ function buildTreatmentsTable(givenSoldier) {
 		   <% }); %>
                 k = 0;
         };
-        node.appendChild(table);
-        document.getElementById("filtersTable").appendChild(node);
+        node.appendChild(table); // Insert table to list element
+        document.getElementById("filtersTable").appendChild(node); // Insert the list element with the table in to the treatments table list in soldiersFiltersTable.ejs
         document.getElementById("chosenSoldiersText").style.visibility = "visible";
     });
 }
 
+/*
+Applies chosen filter on current soldiers table and REBUILD the table acordingly (add/remove constrains - filters ).
+Attention - the function uses POST request that should be handled in index.js
+
+ @param type - the type of the filter. ATTENTION!!! Must to be an EXISTING soldier object field (property) (Status,Division,Unit ... etc)
+ @param value - filter value . ( Example if type is "Status" then value must to be "Severe/Dead/ ... etc")
+ @localPageParam chosenFilters - local array of chosen filters names , to prevent duplicated filters option.
+ 
+*/
 function putFilter(type, value) {
     for (i = 0; i < chosenFilters.length; i++) {
         if (value === chosenFilters[i]) {
@@ -116,6 +133,15 @@ function putFilter(type, value) {
     xhr.open('POST', '/get-soldiers/' + type + '/' + value + '/add', true);
     xhr.send({});
 }
+/*
+
+Builds treatments table for given bracelet ID with the help of buildTreatmentsTable() function.
+Attention - the function uses POST request that should be handled in index.js 
+
+ @param braceletID - the bracelet id of the soldier that we request hes treatments table
+ @localPageParam chosenBracelets - local array of chosen bracelet numbers , to prevent from showing same table twice.
+ 
+*/
 function getTreatments(braceletID) {
     for (i = 0; i < chosenBracelets.length; i++) {
         if (braceletID === chosenBracelets[i]) {
