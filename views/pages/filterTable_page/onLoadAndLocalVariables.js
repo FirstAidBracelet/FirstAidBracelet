928c6445d9fe9916b8b25ee7275fdc39baba220d
alexvitho <alexvitho@gmail.com>
2017-07-18 21:27:09 +0300

    onLoadAndLocalVariables.js documentation

--- views/pages/filterTable_page/onLoadAndLocalVariables.js
+++ views/pages/filterTable_page/onLoadAndLocalVariables.js
@@ -1,14 +1,22 @@
-﻿      /* This is the Socket between back and front end */
-    var socket = io.connect('/'); // connect to server
-  socket.on('news', function (data) { // listen to news event raised by the server
-     buildSoldiersTable(JSON.parse(data));
-  });
+﻿
+/* This is the Socket between back and front end */
+var socket = io.connect('/'); // connect to server
+socket.on('news', function (data) { // listen to news event raised by the server
+    buildSoldiersTable(JSON.parse(data)); // when there any news (say android posted some changes - rebuild the table )
+});
 
-    /*local variables to manipulate chosen data in real time */
-    var chosenFilters = [];
-    var chosenBracelets = [];
-    var filteredSoldiers = [];
+/*local page variables to manipulate chosen data in real time */
+var chosenFilters = []; // to prevent chosing same filter twice
+var chosenBracelets = [];  // to prevent chosing same bracelet twice when building treatments table
+var filteredSoldiers = [];// kind of "cashe" for more eficient management (in relevant functios there is an explanation about this globalPageVariable)
 
+/*
+Function that run on page load
+Building the Soldiers table without any constrains at the very begining/
+Attention - the function uses POST request that should be handled in index.js
+this function use the special "/_/_remove" string in "/get-soldiers/" request to ask for building table without any constrains and filters.
+(We will use this special string for post request to use the existing request , rather than create a new special Post request for creating table witout filters.)
+*/
 window.onload = function () {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
@@ -16,6 +24,6 @@ window.onload = function () {
             buildSoldiersTable(JSON.parse(this.response));
         }
     }
-        xhr.open('POST','/get-soldiers/_/_/remove', true);
-        xhr.send({});
-    };
+    xhr.open('POST', '/get-soldiers/_/_/remove', true);
+    xhr.send({});
+};
